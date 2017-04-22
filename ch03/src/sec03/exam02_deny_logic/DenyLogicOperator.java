b276208a24094b3398ae448fba8a9e6deca30660
CDragon Kim <cdragon.kim@gmail.com>
2017-04-22 22:14:07 +0900

    ch03 두번째 커밋

---
+++ ch03/src/sec03/exam02_deny_logic/DenyLogicOperator.java
@@ -0,0 +1,16 @@
+package sec03.exam02_deny_logic;
+
+public class DenyLogicOperator {
+
+	public static void main(String[] args) {
+		boolean play = true;
+		System.out.println(play);
+		
+		play = !play;
+		System.out.println(play);
+		
+		play = !play;
+		System.out.println(play);
+	}
+
+}
