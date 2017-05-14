2efedc4c16f9da860a93381e84a2a9e1016c3c03
CDragon Kim <cdragon.kim@gmail.com>
2017-05-14 22:18:12 +0900

    Ch07 7.3 부모 생성자 호출

---
+++ ch07/src/sec03/exam01_parent_constructor_call/StudentExample.java
@@ -0,0 +1,13 @@
+package sec03.exam01_parent_constructor_call;
+
+public class StudentExample {
+
+	public static void main(String[] args) {
+		Student student = new Student("홍길동", "12345-33333", 1);
+		System.out.println("name: " + student.name);
+		System.out.println("ssn: " + student.ssn);
+		System.out.println("studentNo: " + student.studentNo);
+
+	}
+
+}
