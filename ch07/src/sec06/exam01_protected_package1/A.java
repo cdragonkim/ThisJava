045d6504e04a82834f155e8a5f6a995cffd47b73
CDragon Kim <cdragon.kim@gmail.com>
2017-05-16 21:39:19 +0900

    7.7 타입변환과 다형성(1)

---
+++ ch07/src/sec06/exam01_protected_package1/A.java
@@ -0,0 +1,13 @@
+package sec06.exam01_protected_package1;
+
+public class A {
+	protected String filed;
+	
+	protected A() {
+		
+	}
+	
+	protected void method() {
+		
+	}
+}
---
+++ ch07/src/sec06/exam01_protected_package2/C.java
@@ -0,0 +1,11 @@
+package sec06.exam01_protected_package2;
+
+import sec06.exam01_protected_package1.A;
+
+public class C {
+	public void method() {
+//		A a = new A();
+//		a.filed = "value";
+//		a.method();
+	}
+}
