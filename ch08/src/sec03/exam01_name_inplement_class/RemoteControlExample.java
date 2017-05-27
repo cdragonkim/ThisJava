7396ce15a635c8606dededa1f94d3acbcfb8141d
CDragon Kim <cdragon.kim@gmail.com>
2017-05-27 11:34:13 +0900

    ~ 8.3

---
+++ ch08/src/sec03/exam01_name_inplement_class/RemoteControlExample.java
@@ -0,0 +1,11 @@
+package sec03.exam01_name_inplement_class;
+
+public class RemoteControlExample {
+
+	public static void main(String[] args) {
+		RemoteControl rc;
+		rc = new Television();
+		rc = new Audio();
+	}
+
+}
